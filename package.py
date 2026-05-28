f635dad5a7dacf65ef3fa4c23b8e462bf6d8cf4e
splidje <jonathan.hearn@ajptechnical.com>
2026-05-28 15:58:00 +0100

    tagged version 0.2.0+ubp0.1.0

--- package.py
+++ package.py
@@ -1,7 +1,7 @@
 """3DEqualizer addon package definition."""
 name = "equalizer"
 title = "3DEqualizer"
-version = "0.2.0+dev"
+version = "0.2.0+ubp0.1.0"
 app_host_name = "equalizer"
 client_dir = "ayon_equalizer"
 project_can_override_addon_version = True
